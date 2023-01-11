638b95fdc356eec6e1e4b818463b8050ad2b73de
Nurul haque Saad <atsaadhaque@gmail.com>
2023-01-11 10:36:28 +0600

    reducer example

--- userefs/src/index.js
+++ userefs/src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import Counter from './reducer/usereducer';
+import Todos from './reducer/usereducer2';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <Counter />
+    <Todos />
   </React.StrictMode>
 );
 
